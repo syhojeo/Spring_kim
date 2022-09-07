18cce7b824503e877022773cdc36ebb9f71c3e7f
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-09-07 16:59:20 +0900

    서비스 객체 구현

---
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,49 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /*회원 가입*/
+    public Long join(Member member) {
+        //같은 이름이 있는 중복 회원X
+        validateDuplicateMember(member); //중복회원 검증
+
+        /*
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+        */
+
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                //값이 있다면 로직 동작 (Optional 을 통해 ifPresent 사용)
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /*
+        전체 회원 조회
+     */
+    public List<Member> findMember() {
+        return memberRepository.findAll();
+    }
+
+    /*
+        id를 통한 조회
+     */
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
