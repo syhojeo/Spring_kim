17901bf5736ddd1d4d30cf7bbabc4d8045b780aa
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-09-07 23:19:11 +0900

    컴포턴트스캔

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,16 +3,20 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
     //dependency Injection (DI)
     //내가 객체를 만드는게 아니라 외부에서 객체를 넣어준다
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
