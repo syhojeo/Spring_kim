f1c4d9ee918126680e93c8987a51af9e476f656f
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-09-08 11:21:14 +0900

    기존의 메모리를 사용하던 레포지토리에서 jdbc를 사용하는 레포지토리로 변경 (OCP)

--- hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,13 +1,26 @@
 package hello.hellospring.service;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
 
 //컴포넌트 스캔이 아닌 직접 Bean 등록
 @Configuration
 public class SpringConfig {
+
+    //스프링에서 DataSource 를 자동주입해준다 (생성자 자동주입)
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +29,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
