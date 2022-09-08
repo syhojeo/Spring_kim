3a95e988f8a7473bcbabeaf77247e788c98e4cfb
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-09-08 09:43:38 +0900

    회원 정보 조회

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -2,9 +2,6 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
@@ -45,7 +42,7 @@ public class MemberService {
     /*
         전체 회원 조회
      */
-    public List<Member> findMember() {
+    public List<Member> findMembers() {
         return memberRepository.findAll();
     }
 
