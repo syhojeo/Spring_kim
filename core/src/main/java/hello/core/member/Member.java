0c87e8ff9e280a06641d955acf1a38eeb4f5eeac
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-09-13 16:44:47 +0900

    스프링없이 회원가입 및 회원조회, 상품주문, 상품할인 기능구현

--- core/src/main/java/hello/core/member/Member.java
+++ core/src/main/java/hello/core/member/Member.java
@@ -1,2 +1,39 @@
-package hello.core.member;public class Member {
+package hello.core.member;
+
+public class Member {
+
+    private Long id;
+    private String name;
+
+    public Member(Long id, String name, Grade gradle) {
+        this.id = id;
+        this.name = name;
+        this.gradle = gradle;
+    }
+
+    private Grade gradle;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Grade getGradle() {
+        return gradle;
+    }
+
+    public void setGradle(Grade gradle) {
+        this.gradle = gradle;
+    }
 }
