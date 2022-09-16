89e81dabad08c1792dd4c7d3794fe9557faa7899
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-09-16 17:53:28 +0900

    싱글톤 문제를 해결하기 위한 공유필드 없애기

--- core/src/test/java/hello/core/singleton/StatefulService.java
+++ core/src/test/java/hello/core/singleton/StatefulService.java
@@ -2,14 +2,16 @@ package hello.core.singleton;
 
 public class StatefulService {
 
-    private int price; //상태를 유지하는 필드
+    //싱글톤 문제를 해결하기 위해 공유 필드 없애기
 
-    public void order(String name, int price) {
+    //private int price; //상태를 유지하는 필드
+
+    public int order(String name, int price) {
         System.out.println("name = " + name + " price = " + price);
-        this.price = price;
-    }
-
-    public int getPrice() {
         return price;
     }
+
+//    public int getPrice() {
+//        return price;
+//    }
 }
