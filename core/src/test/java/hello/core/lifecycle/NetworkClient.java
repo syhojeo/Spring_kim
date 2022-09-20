532227184602b1af6d593faae72ff4eae3ef893c
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-09-20 12:16:27 +0900

    객체 초기화, 소멸 인터페이스와 사용하지 않는 이유

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,6 +1,12 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+//초기화 , 소멸 인터페이스 사용 (Spring에서만 사용가능)
+//외부 라이브러리 같은 경우 적용 불가능
+//위와 같은 단점들이 있기 때문에 사용하지 않는다
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private  String url;
 
@@ -26,5 +32,19 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close " + url);
+    }
+
+    //객체 생성완료 후 실행
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    //객체 소멸완료 후 실행행
+   @Override
+    public void destroy() throws Exception {
+        disconnect();
     }
 }
