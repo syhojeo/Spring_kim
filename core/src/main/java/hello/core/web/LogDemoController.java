a6251974c77b79c64aa1381f8200eee2eda538ad
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2023-03-31 06:55:51 +0900

    request scope
    request가 들어올때 로그를 남기는 객체를 통해 request scope 에 대해서 알아본다

---
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,34 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor //생성자 자동주입 해줌
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    //private final MyLogger myLogger;
+    /*
+        MyLogger를 request Scope 이기 때문에 request 없이 받아올 수 없다 때문에 DL 기능이 필요
+     */
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURI().toString();
+        //myLogger.setRequestURL(requestURL);
+        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testID");
+        return "OK";
+    }
+}
