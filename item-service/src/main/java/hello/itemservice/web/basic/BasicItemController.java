1eaf7ef365dbd100f3f138964db6be1cf53c3809
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2023-04-11 20:35:25 +0900

    타임리프 사용법

---
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,43 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor //Lombok 생성자 주입시 생성자 생략가능 (final 붙어있어야함)
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    //@Autowired 생성자 하나만 있으니 생략 가능
+/*    public BasicItemController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }*/
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     * @PostConstruct 를 이용해 스프링이 bean에 등록하기 위해 컨트롤러를 생성할때 실행된다
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+}
