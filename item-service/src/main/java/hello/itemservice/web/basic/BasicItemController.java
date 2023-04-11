342ada7ee7ecb41d8394f110045877362936db78
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2023-04-11 21:11:45 +0900

    상품 상세 기능 만들기
    - 상품 수정, 목록으로 thymeleaf 코드 추가

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -33,11 +34,19 @@ public class BasicItemController {
 
     /**
      * 테스트용 데이터 추가
+     *
      * @PostConstruct 를 이용해 스프링이 bean에 등록하기 위해 컨트롤러를 생성할때 실행된다
      */
     @PostConstruct
     public void init() {
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
+    }
+
+    @GetMapping("{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
     }
 }
