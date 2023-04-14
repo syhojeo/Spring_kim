7060fb6dfb20ab727f77696c520a7c2c77a0db99
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2023-04-14 23:29:31 +0900

    타임리프 - 멀티 채크 박스 사용법

--- SpringMVC2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ SpringMVC2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -2,6 +2,8 @@ package hello.itemservice.web.form;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -19,6 +21,22 @@ public class FormItemController {
 
     private final ItemRepository itemRepository;
 
+    /*
+        @ModelAttribute 어노테이션을 메서드 위에 지정하면
+        현재 컨트롤러의 어떤 URL 매핑이 요청으로 들어오든
+        해당 메서드가 리턴하는 값이 model 에 자동으로 담기게 된다
+        Model에 리턴하는 값의 이름은 @ModelAttribute("name")의 name이다
+     */
+    @ModelAttribute("regions")
+    public Map<String, String> regions() {
+
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+        return regions;
+    }
+
     @GetMapping
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
@@ -30,12 +48,29 @@ public class FormItemController {
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
+
+        //순서대로 넣기위해 LinkedHashMap 사용
+        //@ModelAttribute 사용으로 주석처리
+/*        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+        model.addAttribute("regions", regions);*/
+
         return "form/item";
     }
 
     @GetMapping("/add")
     public String addForm(Model model) {
         model.addAttribute("item", new Item());
+
+        //@ModelAttribute 사용으로 주석처리
+/*        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+        model.addAttribute("regions", regions);*/
+
         return "form/addForm";
     }
 
@@ -43,6 +78,7 @@ public class FormItemController {
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
         //open 값이 잘오는지 로깅
         log.info("item.open={}", item.getOpen());
+        log.info("item.regions={}", item.getRegions());
 
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
@@ -54,6 +90,16 @@ public class FormItemController {
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
+
+        //@ModelAttribute 사용으로 주석처리
+/*
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+        model.addAttribute("regions", regions);
+*/
+
         return "form/editForm";
     }
 
