4a14ddc9162221a782757e2c3203674718b14495
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2023-04-15 10:41:31 +0900

    타임리프 -셀렉트 박스

--- SpringMVC2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ SpringMVC2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -1,8 +1,10 @@
 package hello.itemservice.web.form;
 
+import hello.itemservice.domain.item.DeliveryCode;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import hello.itemservice.domain.item.ItemType;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
@@ -43,6 +45,19 @@ public class FormItemController {
     public ItemType[] itemTypes() {
         return ItemType.values();
     }
+
+    @ModelAttribute("deliveryCodes")
+    public List<DeliveryCode> deliveryCodes() {
+
+        List<DeliveryCode> deliveryCodes = new ArrayList<>();
+
+        deliveryCodes.add(new DeliveryCode("FAST", "빠른 배송"));
+        deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
+        deliveryCodes.add(new DeliveryCode("SLOW", "느린 배송"));
+
+        return deliveryCodes;
+    }
+
 
     @GetMapping
     public String items(Model model) {
