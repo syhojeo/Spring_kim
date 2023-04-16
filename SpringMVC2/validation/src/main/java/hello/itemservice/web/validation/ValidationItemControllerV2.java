b4fc2f9c104ce62318286144f076d3931254ca5c
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2023-04-16 22:04:21 +0900

    BindingResult.rejectValue(), BindingResult.reject()
    - BindingResult.addError() 를 대체
    
    - rejectValue = FieldError
    
    - reject = ObjectError

--- SpringMVC2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ SpringMVC2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -132,8 +132,12 @@ public class ValidationItemControllerV2 {
 
     //FieldError에서 Codes, arguments 사용 -> defaultMessage 는 null
     //Codes 는 errors.properties에 정의 되어있고, arguments 에는 codes에 필요한 변수를 넣을 수 있다
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        //bindingResult 정보 확인 => target을 Item 으로 알고있다
+        log.info("objectName={}", bindingResult.getObjectName());
+        log.info("target={}", bindingResult.getTarget());
 
         //검증 로직
         //상품 이름이 없는 경우
@@ -168,6 +172,51 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
+    //BindingResult.addError()를 간단히 하기 위해
+    //-> BindingResult.rejectValue()(FieldError), BindingResult.reject()(ObjectError)   로 대체
+    @PostMapping("/add")
+    public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        //bindingResult 정보 확인 => target을 Item 으로 알고있다
+        log.info("objectName={}", bindingResult.getObjectName());
+        log.info("target={}", bindingResult.getTarget());
+
+        //검증 로직
+        //상품 이름이 없는 경우
+        if (!StringUtils.hasText(item.getItemName())) {
+            //bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, new String[]{"required.item.itemName"}, null, null));
+            bindingResult.rejectValue("itemName", "required");
+        }
+        //상품 가격이 너무 낮거나 너무 높은 경우
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            //bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, new String[]{"range.item.price"}, new Object[]{1000, 1000000}, null));
+            bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
+        }
+        //수량 제한
+        if (item.getQuantity() == null || item.getQuantity() > 10000) {
+            //bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, new String[]{"max.item.quantity"}, new Object[]{9999}, null));
+            bindingResult.rejectValue("quantity", "max", new Object[]{9999}, null);
+        }
+        //특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                //bindingResult.addError(new ObjectError("item", new String[]{"totalPriceMin"}, new Object[]{10000, resultPrice},  null));
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+        //검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            //다시 상품 등록 폼으로
+            return "validation/v2/addForm";
+        }
+        //검증 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
 
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
