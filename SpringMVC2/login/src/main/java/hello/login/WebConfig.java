03e6478533bc69262684c29056f962b10a4f0f22
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2023-04-19 23:44:23 +0900

    스프링 인터셉터 사용 - 요청 로그 확인하기

--- SpringMVC2/login/src/main/java/hello/login/WebConfig.java
+++ SpringMVC2/login/src/main/java/hello/login/WebConfig.java
@@ -2,19 +2,31 @@ package hello.login;
 
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
+import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginCheckInterceptor;
 import javax.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+            .order(1)
+            .addPathPatterns("/**")
+            .excludePathPatterns("/css/**", "/*.ico", "/error");
+    }
 
     /*
-        FilterRegistrationBean
-        스프링 부트로 사용시 필터 등록 (스프링 부트가 WAS를 들고 띄우기 때문에 필터를 등록해준다)
-     */
-    @Bean
+            FilterRegistrationBean
+            스프링 부트로 사용시 필터 등록 (스프링 부트가 WAS를 들고 띄우기 때문에 필터를 등록해준다)
+         */
+    //@Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         //등록할 필터
@@ -28,7 +40,7 @@ public class WebConfig {
         return filterFilterRegistrationBean;
     }
 
-    @Bean
+    //@Bean
     public FilterRegistrationBean loginCheckFilter() {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         //등록할 필터
