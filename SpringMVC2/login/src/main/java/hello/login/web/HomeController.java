980b90b76c10c7397a224f414bee2451d4c7fc12
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2023-04-19 14:26:45 +0900

    서비스 코드에 세션 적용시키기

--- SpringMVC2/login/src/main/java/hello/login/web/HomeController.java
+++ SpringMVC2/login/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,8 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionManager;
+import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     private final MemberRepository memberRepository;
+    private final SessionManager sessionManager;
 
     //@GetMapping("/")
     public String home() {
@@ -22,8 +25,8 @@ public class HomeController {
     }
 
     //로그인 안한 회원도 들어와야하기 때문에 required = false
-    @GetMapping("/")
-    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+    //@GetMapping("/")
+    public String homeLoginV1(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
 
         //로그인 안한 경우
         if (memberId == null) {
@@ -38,6 +41,21 @@ public class HomeController {
         }
 
         model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV2(HttpServletRequest request, Model model) {
+
+        //세션 관리자에 저장된 회원 정보 조회
+        Member member = (Member)sessionManager.getSession(request);
+
+        //로그인 안한 경우
+        if (member == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", member);
         return "loginHome";
     }
 }
