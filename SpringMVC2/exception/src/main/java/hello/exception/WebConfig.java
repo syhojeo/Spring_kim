c41792661d9601aa97f4b17487ce8a15ace65a45
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2023-04-20 16:28:02 +0900

    인터셉터를 이용한 예외 요청 처리방법
    - excludePathPatterns를 이용해 원하는 요청에 따른 URL 설정이 가능하고 처리가 가능하다

--- SpringMVC2/exception/src/main/java/hello/exception/WebConfig.java
+++ SpringMVC2/exception/src/main/java/hello/exception/WebConfig.java
@@ -1,16 +1,32 @@
 package hello.exception;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
 import javax.servlet.FilterRegistration;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+            .order(1)
+            .addPathPatterns("/**")
+
+            //인터셉터는 필터처럼 DispatcherType을 확인해서
+            //클라이언트 요청인지, exception 요청인지 구분해서 필터링을 하는게 아니라
+
+            //오류 페이지 경로를 직접 넣어서 해당 URL Mapping 을 막는다
+            //DispatcherType은 확인하지 못하지만 강력한 excludePathPatterns 를 지원
+            .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**");
+    }
 
     @Bean
     public FilterRegistrationBean logFilter() {
