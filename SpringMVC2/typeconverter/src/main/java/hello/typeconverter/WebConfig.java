ff279e4eb7944d9b7d3f136a478df09ded9c46da
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2023-05-12 14:12:37 +0900

    WebConfig에 Formatter 등록하는 방법

--- SpringMVC2/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
+++ SpringMVC2/typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.IpPortToStringConverter;
 import hello.typeconverter.converter.StringToIntegerConverter;
 import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -17,8 +18,12 @@ public class WebConfig implements WebMvcConfigurer {
         //밑의 작업을 통해 스프링 내부에서 사용하는 ConversionService에 Converter를 등록한다
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
 
+        //Formatter 의 우선순위가 converter보다 낮기 때문에 MyNumberFormatter와의 중복 타입으로 인해 주석처리
+        //registry.addConverter(new StringToIntegerConverter());
+        //registry.addConverter(new IntegerToStringConverter());
+
+        //추가 addFormatter
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
