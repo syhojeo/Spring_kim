f26dbb8f07c2f8f62f93d14a05c4a8433d3af72a
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2023-05-17 16:58:51 +0900

    JDBC 개발 - 조회 (ResultSet - executeQuery())

--- SpringDB1/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ SpringDB1/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -7,6 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.NoSuchElementException;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -41,6 +42,41 @@ public class MemberRepositoryV0 {
         } finally {
             //쿼리 실행 후 리소스 정리
             close(con, pstmt, null);
+        }
+    }
+
+    //Member id를 이용하여 쿼리 조회하기
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            //sql 세팅
+            pstmt.setString(1, memberId);
+
+            //executeQuery() 를 통해 조회한 정보가 담긴 ResultSet을 받아온다
+            rs = pstmt.executeQuery();
+            //ResultSet은 next() 메서드를 통해 iterator 한다
+            //조회할 row가 2개 이상이라면 while을 통해 iterator 하면 된다
+            if (rs.next()) {
+                Member member = new Member();
+                //getString, getInt 등을 사용하여 해당 row 의 column을 통해 데이터를 조회할 수 있다
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId=" + memberId);
+            }
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, rs);
         }
     }
 
--- SpringDB1/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ SpringDB1/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,18 +1,48 @@
 package hello.jdbc.repository;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import hello.jdbc.domain.Member;
 import java.sql.SQLException;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
-        Member member = new Member("memberV0", 10000);
+        //save
+        Member member = new Member("memberV2", 10000);
         repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        //log.info("member != findMember {}", member == findMember);
+        //log.info("member == findMember {}", member.equals(findMember));
+        assertThat(findMember).isEqualTo(member);
+
+        /*
+            @Data - Member
+            Member 객체의 Lombok의 @Data 애노테이션이 사용되어 다음과 같은 효과를 가져온다
+
+            log.info("findMember={}", findMember); - toString 이 구현됨
+            findMember를 log로 출력했을때 객체의 주소가 출력되는것이 아니라
+            findMember의 멤버 변수가 순서대로 출력된다
+
+            log.info("member != findMember {}", member == findMember); 를 찍었을때에는 객체의 주소가
+            다르기 때문에 false가 출력되지만
+
+            log.info("member == findMember {}", member.equals(findMember)); - EqualsAndHashCode 이 구현
+            EqualsAndHashCode가 구현되어 있기 때문에 객체의 멤버 변수를 비교하여 true가 출력되게 해준다
+            isEqualTo 에도 똑같이 적용된다
+
+            @Data는 사용하면 Test코드를 매우 편리하게 작성할 수 있도록 해준다
+         */
     }
 }
