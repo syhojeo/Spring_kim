3c25284d4cb11a05eae93b981257711d2ef6c025
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2023-05-27 17:26:23 +0900

    커넥션 풀을 이용한 DataSource 사용

--- SpringDB1/jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ SpringDB1/jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -2,6 +2,7 @@ package hello.jdbc.connection;
 
 import static hello.jdbc.connection.ConnectionConst.*;
 
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -22,6 +23,28 @@ public class ConnectionTest {
         log.info("conneciton={}, class={}", con2, con2.getClass());
     }
 
+    //DataSource(HikariCP - Connection Pool)를 이용하여 커넥션 가져오기
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        //커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10); //기본이 10
+        dataSource.setPoolName("MyPool"); //connection pool 이름 지정
+
+        useDataSource(dataSource);
+        /*
+           풀에서 커넥션을 생성하는 것은 현재 메서드를 실행하는 스레드와 다른 별도의 스레드에서 동작하기 때문에
+           커넥션을 생성하는 로그를 확인하기 위해서는 sleep 메서드를 사용하여
+           dataSourceConnectionPool(현재 메서드) 메서드를 실행하는 스레드가 종료되지 않도록 해야 로그를 확인할 수 있다
+         */
+
+        Thread.sleep(1000);
+    }
+
+    //DataSource(DriverManager)를 이용하여 커넥션 가져오기
     @Test
     void dataSourceDriverManager() throws SQLException {
         //DriverManagerDataSource - 항상 새로운 커넥션을 획득 (DataSource를 구현한 DriverManager 구현체)
